e8116b2b5919c9673a94c26eac8c9e425fac4931
ManneyLucy <nwalusiemmanuel7@gmail.com>
2022-12-15 01:26:50 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
+ */
+
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int, int);
+void print_number(void);
+void print_most_number(void);
+void more_number(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif
